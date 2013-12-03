47fd19873ee256d31c9ebb67120b0328075da2a8
nisbus <nisbus@gmail.com>
2013-12-03 22:30:25 +0000

    First commit of 4.6

---
+++ RXUIPerformance/RXClass.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReactiveUI;
+
+namespace RXUIPerformance
+{
+    public class RXClass : ReactiveObject, INotifyPropertyChanged
+    {
+        private int _RXSetter = 0;
+        public int RXSetter
+        {
+            get { return _RXSetter; }
+            set { this.RaiseAndSetIfChanged(ref _RXSetter, value); }
+        }
+
+        private int _OldSetter = 0;
+        public int OldSetter
+        {
+            get { return _OldSetter; }
+            set { this.RaiseAndSetIfChanged(x => x.OldSetter, value); }
+        }
+    }
+
+    public class PlainWPFWithAttachedPropertyChangeEvents : INotifyPropertyChanged, INotifyPropertyChanging
+    {        
+        private int _NotifySetter = 0;
+        public int NotifySetter
+        {
+            get { return _NotifySetter; }
+            set
+            {
+                _NotifySetter = value;
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("NotifySetter"));
+            }
+        }
+
+        private int _ChangingAndChangedSetter = 0;
+        public int ChangingAndChangedSetter
+        {
+            get { return _ChangingAndChangedSetter; }
+            set 
+            {
+                if (PropertyChanging != null)
+                    PropertyChanging(this, new PropertyChangingEventArgs("ChangingAndChangedSetter"));
+                _ChangingAndChangedSetter = value;
+                if(PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("ChangingAndChangedSetter"));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged = (_, __) => { };
+        public event PropertyChangingEventHandler PropertyChanging = (_, __) => { };
+    }
+
+    public class PlainWPFWithoutAttachedPropertyChangeEvents : INotifyPropertyChanged, INotifyPropertyChanging
+    {
+        private int _NotifySetter = 0;
+        public int NotifySetter
+        {
+            get { return _NotifySetter; }
+            set
+            {
+                _NotifySetter = value;
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("NotifySetter"));
+            }
+        }
+
+        private int _ChangingAndChangedSetter = 0;
+        public int ChangingAndChangedSetter
+        {
+            get { return _ChangingAndChangedSetter; }
+            set
+            {
+                if (PropertyChanging != null)
+                    PropertyChanging(this, new PropertyChangingEventArgs("ChangingAndChangedSetter"));
+                _ChangingAndChangedSetter = value;
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("ChangingAndChangedSetter"));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public event PropertyChangingEventHandler PropertyChanging;
+    }
+
+    public class PlainClass
+    {
+        private int _PlainSetter = 0;
+        public int PlainSetter
+        {
+            get { return _PlainSetter; }
+            set { _PlainSetter = value; }
+        }
+
+        public int AutoSetter { get; set; }
+    }
+}
---
+++ RXUIPerformance/UnitTest1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Reactive.Concurrency;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Reactive;
+using System.Reactive.Linq;
+using ReactiveUI;
+
+namespace RXUIPerformance
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestInitializeAttribute]
+        public void Init()
+        {
+
+            //RxApp.DeferredScheduler = Scheduler.TaskPool;
+        }
+
+        int TestCount = 100000;
+        [TestMethod]
+        public void TestSettingOldRXStyle()
+        {
+            var RX = new RXClass();
+            for (int i = 0; i < TestCount; i++)
+                RX.OldSetter = i;
+        }
+
+        [TestMethod]
+        public void TestSettingNewRXStyle()
+        {
+            var RX = new RXClass();
+            for (int i = 0; i < TestCount; i++)
+                RX.RXSetter = i;
+        }
+
+        [TestMethod]
+        public void TestSettingWPFAttachedChangedOnly()
+        {
+            var WPF = new PlainWPFWithAttachedPropertyChangeEvents();
+            for (int i = 0; i < TestCount; i++)
+                WPF.NotifySetter = i;
+        }
+
+        [TestMethod]
+        public void TestSettingWPFBothAttached()
+        {
+            var WPF = new PlainWPFWithAttachedPropertyChangeEvents();
+            for (int i = 0; i < TestCount; i++)
+                WPF.ChangingAndChangedSetter = i;
+        }
+
+        [TestMethod]
+        public void TestSettingWPFNonAttachedChangedOnly()
+        {
+            var WPF = new PlainWPFWithoutAttachedPropertyChangeEvents();
+            for (int i = 0; i < TestCount; i++)
+                WPF.NotifySetter = i;
+        }
+
+        [TestMethod]
+        public void TestSettingWPFBothNonAttached()
+        {
+            var WPF = new PlainWPFWithoutAttachedPropertyChangeEvents();
+            for (int i = 0; i < TestCount; i++)
+                WPF.ChangingAndChangedSetter = i;
+        }
+
+        [TestMethod]
+        public void TestSettingPlain()
+        {
+            var Plain = new PlainClass();
+            for (int i = 0; i < TestCount; i++)
+                Plain.PlainSetter = i;
+        }
+
+        [TestMethod]
+        public void TestSettingPlainAuto()
+        {
+            var Plain = new PlainClass();
+            for (int i = 0; i < TestCount; i++)
+                Plain.AutoSetter = i;
+        }
+    }
+}
